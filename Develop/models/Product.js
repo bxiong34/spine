5633ec153c350c20af38205ccf88680718dabfb8
14089 <baoxiong814@gmail.com>
2023-12-07 23:08:04 -0600

    fixed product routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -21,16 +21,17 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
-      validate: {isDecimal: true,
+      validate: {
+        isDecimal: true,
       },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
       validate: {
-        len: [10],
         isNumeric: true,
       },
     },
